1ec7b289e7731e6627c314ee579b7ddf2615936e
Dwight <38108827+dlhvelasco@users.noreply.github.com>
2018-11-29 16:21:12 +0800

    Revert "0.5 threshold"
    This reverts commit 866ec1b12bf4634ca6676df3226690a5864cac75.

--- print_results.py
+++ print_results.py
@@ -1,5 +1,5 @@
 import subprocess
 
 print("Printing final results...\n")
-finalresult = ['python',r'C:/Users/Dwight/Music/AP187-Imaging/Dermatologist-AI/get_results.py',r'C:/Users/Dwight/Music/AP187-Imaging/Dermatologist-AI/predictions.csv','0.5']
+finalresult = ['python',r'C:/Users/Dwight/Music/AP187-Imaging/Dermatologist-AI/get_results.py',r'C:/Users/Dwight/Music/AP187-Imaging/Dermatologist-AI/predictions.csv','0.4']
 finalresultcomplete = subprocess.run(finalresult, shell=True)
